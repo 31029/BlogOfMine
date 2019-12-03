38fd6acc590df8ef44de29df649c5abbd7bb25dc
31029 <740876073@qq.com>
2019-12-03 21:05:03 +0800

    Before Heroku

---
+++ users/urls.py
@@ -0,0 +1,20 @@
+"""Defines url patterns for users."""
+
+from django.conf.urls import url
+
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+app_name='users'
+urlpatterns = [
+    # Login page.
+    url(r'^login/$', LoginView.as_view(template_name='users/login.html'),
+        name='login'),
+        
+    # Logout page.
+    url(r'^logout/$', views.logout_view, name='logout'),
+    
+    # Registration page. 
+    url(r'^register/$', views.register, name='register'),
+]

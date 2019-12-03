e2a76a77f9a1f9d6ceaa7b3ea98c3e6496d3f4ee
31029 <740876073@qq.com>
2019-12-03 21:52:28 +0800

    Before Heroku 2

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -126,7 +126,25 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# 我的设置 
-LOGIN_URL = '/users/login/' 
-# django-bootstrap3的设置 
-BOOTSTRAP3 = {    'include_jquery': True,    }
+# 我的设置
+LOGIN_URL = '/users/login/'
+# django-bootstrap3的设置
+BOOTSTRAP3 = {'include_jquery': True, }
+
+#HeroKu设置
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+    # 让request.is_secure()承认X-Forwarded-Proto头
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # 支持所有的主机头（host header）
+    ALLOWED_HOSTS = ['*']
+
+    # 静态资产配置
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
